b42745873b71373175636588cdf0d58fa473334c
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2013-09-21 01:10:42 -0400

    codeeval.com - easy 2 - Prime Palindrome

---
+++ codeeval.com/easy/PrimePalindrome.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+class PrimePalindromeMain
+{
+	public static void Main (string[] args)
+	{
+		using (StreamWriter output = new StreamWriter (Console.OpenStandardOutput())) {
+
+			var problem = new PrimePalindrome (output);
+			problem.Run ();
+		}
+	}
+}
+
+public class PrimePalindrome
+{
+	StreamWriter output;
+
+	public PrimePalindrome (StreamWriter output)
+	{
+		this.output = output;
+	}
+
+	public void Run()
+	{
+		var primes = Primes (1000).GetEnumerator();
+		int palindrome = 0;
+
+		while (primes.MoveNext()) {
+			int number = primes.Current;
+			if (number == Reverse (number))
+				palindrome = number;
+		}
+
+		output.WriteLine(palindrome);
+	}
+
+	int Reverse (int number)
+	{
+		char[] charArray = number.ToString().ToCharArray();
+		Array.Reverse( charArray );
+		string reverse = new string( charArray );
+
+		return int.Parse(reverse);
+	}
+
+	public IEnumerable<int> Primes( int max )
+	{
+		yield return 2;
+		List<int> found = new List<int>();
+		found.Add( 3 );
+		int candidate = 3;
+		while ( candidate <= max )
+		{
+			bool isPrime = true;
+			foreach ( int prime in found )
+			{
+				if ( prime * prime > candidate )
+				{
+					break;
+				}
+				if ( candidate % prime == 0 )
+				{
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime)
+			{
+				found.Add( candidate );
+				yield return candidate;
+			}
+			candidate += 2;
+		}
+	}
+}
