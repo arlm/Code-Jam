a6262934a414b46d51f89d7cb38f4e2df3a1eb65
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2013-09-20 23:38:56 -0400

    Optimization

--- codeeval.com/easy/FizzBuzz.cs
+++ codeeval.com/easy/FizzBuzz.cs
@@ -2,64 +2,72 @@ using System;
 using System.IO;
 using System.Text;
 
-namespace codeeval.com
+class Program
 {
-	public class FizzBuzz
+	public static void Main (string[] args)
 	{
-		StreamReader input;
-		StreamWriter output;
+		string inputPath = "../../input/";
+		string outputPath = "../../output/";
+		string fileName = "easy-1-fizz-buzz.txt";
 
-		int counter = 0;
-		int A = 1;
-		int B = 1;
+		using (StreamReader input = File.OpenText(inputPath + fileName)) {
+			//using (StreamReader input = File.OpenText(args[0])) {
 
-		public FizzBuzz (StreamReader input, StreamWriter output)
-		{
-			this.input = input;
-			this.output = output;
-		}
+			using (StreamWriter output = new StreamWriter (outputPath + fileName)) {
+				// using (StreamWriter output = new StreamWriter (Console.OpenStandardOutput())) {
 
-		private bool Fizz(int number) {
-			return number % A == 0;
-		}
-
-		private bool Buzz(int number) {
-			return number % B == 0;
-		}
-
-		public void Run()
-		{
-			string line = input.ReadLine ();
-			while (!string.IsNullOrEmpty(line)) {
-				StringBuilder sb = new StringBuilder();
-				string[] elements = line.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-
-				A = int.Parse(elements[0]);
-				B = int.Parse(elements[1]);
-				counter = int.Parse(elements[2]);
-
-				for (int i = 1; i <= counter; i++) {
-					bool f = Fizz (i);
-					bool b = Buzz(i);
-
-					if (f) {
-						sb.Append ("F");
-					}
-					if (b) {
-						sb.Append ("B");
-					}
-					if (!f && !b) {
-						sb.Append (i);
-					}
-					if (i < counter) {
-						sb.Append (" ");
-					}
-				}
-
-				output.WriteLine(sb.ToString());
-				line = input.ReadLine ();
+				var problem = new FizzBuzz (input, output);
+				problem.Run ();
 			}
 		}
 	}
 }
 
+public class FizzBuzz
+{
+	StreamReader input;
+	StreamWriter output;
+
+	int counter = 0;
+	int A = 1;
+	int B = 1;
+
+	public FizzBuzz (StreamReader input, StreamWriter output)
+	{
+		this.input = input;
+		this.output = output;
+	}
+
+	public void Run()
+	{
+		while (!input.EndOfStream) {
+			string line = input.ReadLine ();
+			StringBuilder sb = new StringBuilder();
+			string[] elements = line.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+			A = int.Parse(elements[0]);
+			B = int.Parse(elements[1]);
+			counter = int.Parse(elements[2]);
+
+			for (int number = 1; number <= counter; number++) {
+				bool f = number % A == 0;
+				bool b = number % B == 0;
+
+				if (f) {
+					sb.Append ("F");
+				}
+				if (b) {
+					sb.Append ("B");
+				}
+				if (!f && !b) {
+					sb.Append (number);
+				}
+				if (number < counter) {
+					sb.Append (" ");
+				}
+			}
+
+			output.WriteLine(sb.ToString());
+		}
+	}
+}
