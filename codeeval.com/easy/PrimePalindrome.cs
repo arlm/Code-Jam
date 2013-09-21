265136725c4bf9095348d665a609482e855607b3
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2013-09-21 19:01:50 -0400

    Added gists

--- codeeval.com/easy/PrimePalindrome.cs
+++ codeeval.com/easy/PrimePalindrome.cs
@@ -2,15 +2,16 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using System.Collections;
 
 class PrimePalindromeMain
 {
 	public static void Main (string[] args)
 	{
-		using (StreamWriter output = new StreamWriter (Console.OpenStandardOutput())) {
+		using (StreamWriter output = new StreamWriter(Console.OpenStandardOutput())) {
 
-			var problem = new PrimePalindrome (output);
-			problem.Run ();
+			var problem = new PrimePalindrome(output);
+			problem.Run();
 		}
 	}
 }
@@ -19,19 +20,19 @@ public class PrimePalindrome
 {
 	StreamWriter output;
 
-	public PrimePalindrome (StreamWriter output)
+	public PrimePalindrome(StreamWriter output)
 	{
 		this.output = output;
 	}
 
-	public void Run()
+	public void Run ()
 	{
-		var primes = Primes (1000).GetEnumerator();
+		var primes = BruteForcePrimes(1000).GetEnumerator ();
 		int palindrome = 0;
 
 		while (primes.MoveNext()) {
 			int number = primes.Current;
-			if (number == Reverse (number))
+			if (number == Reverse(number))
 				palindrome = number;
 		}
 
@@ -40,37 +41,36 @@ public class PrimePalindrome
 
 	int Reverse (int number)
 	{
-		char[] charArray = number.ToString().ToCharArray();
-		Array.Reverse( charArray );
-		string reverse = new string( charArray );
+		char[] charArray = number.ToString().ToCharArray ();
+		Array.Reverse (charArray);
+		string reverse = new string(charArray);
 
-		return int.Parse(reverse);
+		return int.Parse (reverse);
 	}
 
-	public IEnumerable<int> Primes( int max )
+	public IEnumerable<int> BruteForcePrimes(int max)
 	{
+		if (max < 2) yield break;
 		yield return 2;
+
 		List<int> found = new List<int>();
-		found.Add( 3 );
+
+		found.Add (3);
 		int candidate = 3;
-		while ( candidate <= max )
-		{
+
+		while (candidate <= max) {
 			bool isPrime = true;
-			foreach ( int prime in found )
-			{
-				if ( prime * prime > candidate )
-				{
+			foreach (int prime in found) {
+				if (prime * prime > candidate) {
 					break;
 				}
-				if ( candidate % prime == 0 )
-				{
+				if (candidate % prime == 0) {
 					isPrime = false;
 					break;
 				}
 			}
-			if (isPrime)
-			{
-				found.Add( candidate );
+			if (isPrime) {
+				found.Add (candidate);
 				yield return candidate;
 			}
 			candidate += 2;
