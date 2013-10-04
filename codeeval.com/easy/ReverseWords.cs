8577f0ba1268c31376df8e69e41ccd42fd33725f
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2013-10-04 17:40:07 -0400

    CodeEval.com Easy challenge 4 - Reverse Words

--- codeeval.com/easy/ReverseWords.cs
+++ codeeval.com/easy/ReverseWords.cs
@@ -2,11 +2,11 @@ using System;
 using System.IO;
 using System.Text;
 
-class FizzBuzzMain {
+public class ReverseWordsMain {
     public static void Main(string[] args) {
         string inputPath = "../../input/";
         string outputPath = "../../output/";
-        string fileName = "easy-1-fizz-buzz.txt";
+        string fileName = "easy-4-reverse-words.txt";
 
         using (StreamReader input = File.OpenText(inputPath + fileName)) {
             //using (StreamReader input = File.OpenText(args[0])) {
@@ -14,21 +14,18 @@ class FizzBuzzMain {
             using (StreamWriter output = new StreamWriter(outputPath + fileName)) {
                 // using (StreamWriter output = new StreamWriter (Console.OpenStandardOutput())) {
 
-                var problem = new FizzBuzz(input, output);
+                var problem = new ReverseWords(input, output);
                 problem.Run();
             }
         }
     }
 }
 
-public class FizzBuzz {
+public class ReverseWords {
     StreamReader input;
     StreamWriter output;
-    int counter = 0;
-    int A = 1;
-    int B = 1;
 
-    public FizzBuzz(StreamReader input, StreamWriter output) {
+    public ReverseWords(StreamReader input, StreamWriter output) {
         this.input = input;
         this.output = output;
     }
@@ -39,29 +36,13 @@ public class FizzBuzz {
             StringBuilder sb = new StringBuilder();
             string[] elements = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            A = int.Parse(elements[0]);
-            B = int.Parse(elements[1]);
-            counter = int.Parse(elements[2]);
-
-            for (int number = 1; number <= counter; number++) {
-                bool f = number % A == 0;
-                bool b = number % B == 0;
-
-                if (f) {
-                    sb.Append("F");
-                }
-                if (b) {
-                    sb.Append("B");
-                }
-                if (!f && !b) {
-                    sb.Append(number);
-                }
-                if (number < counter) {
-                    sb.Append(" ");
-                }
+            for (int index = elements.Length - 1; index >= 0; index--) {
+                sb.Append(elements[index]);
+                sb.Append(" ");
             }
 
             output.WriteLine(sb.ToString());
         }
     }
 }
+
