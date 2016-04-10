6aea05b4a277a7203e29c137893ee9ed9fa705bb
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2016-04-10 18:41:28 +0200

    Code Jam 2016

---
+++ 2016/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Threading;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            const string inputPath = "../../input/";
+            const string outputPath = "../../output/";
+            const string fileName = "qr-a-sample.txt";
+
+            var input = File.OpenText(inputPath + fileName);
+            var output = new StreamWriter(outputPath + fileName);
+
+            var problem = new QR_A(input, output);
+            problem.Run();
+
+            input.Close();
+            output.Close();
+        }
+    }
+}
---
+++ 2016/QR_A.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Application
+{
+    public class QR_A
+    {
+        readonly StreamReader input;
+        readonly StreamWriter output;
+
+        readonly int counter;
+
+        public QR_A (StreamReader input, StreamWriter output)
+        {
+            this.input = input;
+            this.output = output;
+
+            counter = int.Parse(input.ReadLine());
+        }
+
+        public QR_A ()
+        {
+        }
+
+        public void Run()
+        {
+            for (int index = 1; index <= counter; index++) {
+                var value = input.ReadLine ();
+                int intValue = int.Parse (value);
+
+                bool[] digits = { false, false, false, false, false,
+                    false, false, false, false, false};
+
+                int iteration = 1;
+                int iterationValue;
+                do
+                {
+                    if (iteration > 10000)
+                        break;
+
+                    iterationValue = intValue * iteration;
+                    iteration++;
+                    foreach (char digit in iterationValue.ToString())
+                    {
+                        digits[(int)char.GetNumericValue(digit)] = true;
+                    }
+                } while (digits.Count(d => d) < 10);
+
+
+                var sb = new StringBuilder();
+                if (iteration > 10000)
+                {
+                    sb.AppendFormat("Case #{0}: INSOMNIA", index);
+                }
+                else
+                {
+                    sb.AppendFormat("Case #{0}: {1}", index, iterationValue);
+                }
+
+                output.WriteLine(sb);
+            }
+        }
+    }
+}
+
